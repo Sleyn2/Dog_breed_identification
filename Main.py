fe0ea3998c6590100565353d5d88a6ba1860352b
Mateusz Marczewski <43947266+Sleyn2@users.noreply.github.com>
2021-06-08 20:50:23 +0200

    Loading files and listing basic info

--- Main.py
+++ Main.py
@@ -1,4 +1,25 @@
-import tensorflow
+import pandas
+import sklearn
 
-print("hello worsdfsfdld!")
-#TODO: add code here
+from os.path import join
+from os import listdir
+from sklearn.model_selection import train_test_split
+# 1 Parameters
+# TODO: set parameters
+
+# 2 Importing dataset
+Num_classes = 10
+
+data_dir = 'input/'
+labels = pandas.read_csv(join(data_dir, 'labels.csv'))
+
+print("Calkowita ilosc obrazow treningowych: {}".format(len(listdir(join(data_dir, 'train')))))
+# Wyswietlanie Num_classes - ilości ras
+print("Pierwsze {} ras".format(Num_classes))
+print(labels
+      .groupby("breed")
+      .count()
+      .sort_values("id", ascending=False)
+      .head(Num_classes)
+      )
+# TODO: add cross validation
