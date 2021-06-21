83622ced0dbcf82fb8f8e4540009ede15c0364ac
Jan-Gabzdyl <jan.gabzdyl1999@gmail.com>
2021-06-21 18:47:53 +0200

    prediction in main file

--- Breed_Identification.py
+++ Breed_Identification.py
@@ -6,6 +6,7 @@ from keras.layers import Dense, GlobalAveragePooling2D, Dropout
 from keras.models import Sequential
 from keras.preprocessing.image import ImageDataGenerator as Imgen
 from tensorflow import keras
+from keras.models import load_model
 
 
 NUM_CLASSES = 10
@@ -28,11 +29,24 @@ def plot_images(img, labels):
         plt.axis('off')
 
 
+def check_images(img):
+    plt.figure(figsize=[25, 10])
+    for i in range(25):
+        plt.subplot(5, 5, i + 1)
+        plt.imshow(img[i])
+        plt.title(class_names[pred[i]])
+        plt.axis('off')
+
+
+test_path = 'input/test'
 labels_path = 'input/labels.csv'
 train_path = 'input/train'
+sample_path = 'input/sample_submission.csv'
 
+samples = pandas.read_csv(sample_path)
 labels = pandas.read_csv(labels_path)
 labels['id'] = labels['id'].apply(app_jpg)
+samples['id'] = samples['id'].apply(app_jpg)
 
 datagen = Imgen(preprocessing_function=keras.applications.nasnet.preprocess_input,
                 shear_range=0.2,
@@ -90,7 +104,7 @@ model = Sequential([
     Dropout(0.5),
     Dense(120, activation='softmax')
 ])
-# model.summary()
+model.summary()
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 my_calls = [keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=2),
@@ -114,3 +128,26 @@ plt.plot(hist.epoch, hist.history['val_loss'], label='validation')
 plt.title("Loss")
 plt.legend()
 plt.show()
+
+# model = load_model("./Model.h5")
+#
+# testgen = Imgen(preprocessing_function=keras.applications.inception_resnet_v2.preprocess_input)
+#
+# test_dataset = testgen.flow_from_dataframe(
+#     samples,
+#     directory=test_path,
+#     x_col='id',
+#     y_col=None,
+#     target_size=(IMAGE_SIZE, IMAGE_SIZE),
+#     class_mode=None,
+#     batch_size=BATCH_SIZE,
+#     shuffle=False
+# )
+#
+# predictions = model.predict(test_dataset, verbose=1)
+# pred = [np.argmax(i) for i in predictions]
+#
+# X = next(test_dataset)
+# X.shape
+#
+# check_images(X)
